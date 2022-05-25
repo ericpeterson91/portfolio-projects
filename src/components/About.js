35afd6d1250fe2297e7f7a082852ecf531c6cb84
Eric Peterson <ericpeterson91@live.ca>
2022-05-25 01:05:23 -0600

    icons

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import './About.css'
 import me from '../images/Eric.jpeg'
+import { FaGithub, FaLinkedin, FaReact, FaNode, FaJsSquare, FaCss3Alt, FaHtml5 } from "react-icons/fa";
 
 const About = () => {
   return (
     <div className='about-container'>
       <img src={me} alt="" />
+      <FaGithub />
+      <FaLinkedin />
+      <FaReact />
+      <FaNode />
+      <FaJsSquare />
+      <FaCss3Alt />
+      <FaHtml5 />
     </div>
     
   )
