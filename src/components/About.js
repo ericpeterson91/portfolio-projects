5798f2cf0de5be3f87507bf0ebe7bf69264e0daf
Eric Peterson <ericpeterson91@live.ca>
2022-06-07 19:05:25 -0600

    bio section

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ const About = () => {
       </div>
       <div className='about'>
         <h1>About Eric</h1>
-        <p> nesciunt adip fugit similique dicta doloremque aliquam earum exercitationem omnis recusandae ad doloribus ullam debitis, aut consequuntur hic cum repudiandae inventore placeat autem. Ad nulla quas dolore rerum illo molestias ea in cum libero a. Ex eius ut placeat quisquam, harum eos magni labore dolorem reprehenderit necessitatibus neque recusandae nisi quaerat possimus, asperiores sunt fugit sit rerum voluptas amet laudantium velit magnam maiores assumenda. Quos iusto dolores dolorum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione vitae, veniam accusamus placeat ullam deleniti quos! Distinctio possimus numquam esse?</p>
+        <p> Eric is full stack web developer from Saskatoon, Saskatchewan. <br /> <br />Eric is passionate about the world of JavaScript, and the world of software development more generally. <br /><br /> Eric is open to work in Saskatoon, relocating cities for work, and also full-time remote work.</p>
       </div>
       <div className='logo-container'>
         
