e20f0848e4501b890e6ead978aacfb65fb635090
Eric Peterson <ericpeterson91@live.ca>
2022-06-07 20:00:12 -0600

    email section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
-import { FaLinkedin, FaGithub} from 'react-icons/fa'
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 import emailjs from "@emailjs/browser";
+import "./Contact.css";
 
 const Contact = () => {
   const form = useRef();
@@ -27,31 +28,37 @@ const Contact = () => {
   };
 
   return (
-    <div className='contact-container'>
-      <form className='form' ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="name" />
-        <label>Subject</label>
-        <input type="text" name="subject" />
-        <label>Email</label>
-        <input type="email" name="email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
-      </form>
-      <div className="icons">
+    <div className="contact-container">
+      <div className="form-container">
+        <h2>Send Eric an email below:</h2>
+        <form className="form" ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="name" />
+          <label>Subject</label>
+          <input type="text" name="subject" />
+          <label>Email</label>
+          <input type="email" name="email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+      <div className="icon-container">
+        <h2>Links:</h2>
+        <div className="icons">
         <a
           target="_blank"
           href="https://github.com/ericpeterson91?tab=repositories"
         >
-          <FaGithub size={25} />
+          <FaGithub size={45} className='github'/>
         </a>
         <a
           target="_blank"
           href="https://www.linkedin.com/in/eric-peterson-110a62162/"
         >
-          <FaLinkedin size={25} />
+          <FaLinkedin size={45} className='linkedin' />
         </a>
+        </div>
       </div>
     </div>
   );
