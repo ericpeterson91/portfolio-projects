f9b6ee6a34b85e436223217a99e4f0a416c00a3d
Eric Peterson <ericpeterson91@live.ca>
2022-06-13 03:14:19 -0600

    styling

--- src/components/Project.js
+++ src/components/Project.js
@@ -15,16 +15,20 @@ const Project = ({
       <div className="name">{name}</div>
 
       <div className="desc">{description}</div>
-      <div className="login-container">
-        <div className="guest">{guestLogin}</div>
-        <div className="guestD">{guestDesc}</div>
-        <div className="guestP">{guestP}</div>
-      </div>
+
       <div className="img-container">
         <img src={src} className="src" />
-        <a className="p-link" target="_blank" href={link}>
-          Link to project
-        </a>
+
+        <div className="login-container">
+          <div className='guest-container'>
+            <div className="guest">{guestLogin}</div>
+            <div className="guestD">{guestDesc}</div>
+            <div className="guestP">{guestP}</div>
+          </div>
+          <a className="p-link" target="_blank" href={link}>
+            Link to project
+          </a>
+        </div>
       </div>
     </div>
   );
