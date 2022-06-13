7299bf59686df48968bf22e4e863b689993bda61
Eric Peterson <ericpeterson91@live.ca>
2022-06-13 00:43:37 -0600

    styling

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,20 +1,33 @@
-import React from 'react'
-import './Project.css'
+import React from "react";
+import "./Project.css";
 
-const Project = ({name, src, description, link, guestLogin, guestDesc, guestP}) => {
+const Project = ({
+  name,
+  src,
+  description,
+  link,
+  guestLogin,
+  guestDesc,
+  guestP,
+}) => {
   return (
-    <div className='project-card'>
-        <div className='name'>{name}</div>
-        <img src={src} className='src' />
-        <div className='desc'>{description}</div>
-        <div className='login-container'>
-          <div className='guest'>{guestLogin}</div>
-          <div className='guestD'>{guestDesc}</div>
-          <div className='guestP'>{guestP}</div>
-        </div>  
-        <a className='p-link' target="_blank" href={link}>Link to project</a>
-    </div>
-  )
-}
+    <div className="project-card">
+      <div className="name">{name}</div>
 
-export default Project
+      <div className="desc">{description}</div>
+      <div className="login-container">
+        <div className="guest">{guestLogin}</div>
+        <div className="guestD">{guestDesc}</div>
+        <div className="guestP">{guestP}</div>
+      </div>
+      <div className="img-container">
+        <img src={src} className="src" />
+        <a className="p-link" target="_blank" href={link}>
+          Link to project
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
