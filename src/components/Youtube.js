479f0459d4acab63dd10a12e4412878232867125
Eric Peterson <ericpeterson91@live.ca>
2022-11-15 04:39:12 -0700

    embed

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -12,9 +12,9 @@ const Youtube = () => {
     <div className="hero-container youtube-container">
       <div className='youtube-cards y-cards'>
         <Video src="https://www.youtube.com/embed/MjbC0gF22kg"  title="React Components, Props, and State"/>
-        <Video src="https://www.youtube.com/embed/SrCTSnXdGp8"  title="Indexing into an Array at Random"/>
+        <Video src="https://www.youtube.com/embed/tLzMVZWapu8"  title="Changing Color in JavaScript"/>
         <Video src="https://www.youtube.com/embed/8PYEU-a409c"  title="CSS Smooth Transitions"/>
-        <Video src="https://www.youtube.com/embed/Kgp5U-2ySAY"  title="Changing Color in JavaScript"/>
+        <Video src="https://www.youtube.com/embed/SrCTSnXdGp8"  title="Indexing into an Array at Random"/>
       </div>
     </div>
   );
