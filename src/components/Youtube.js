9c0a1050deb00677680a4de322750f81107dac90
Eric Peterson <ericpeterson91@live.ca>
2022-11-25 10:32:00 -0700

    updating yt videos

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -11,10 +11,12 @@ const Youtube = () => {
   return (
     <div className="hero-container youtube-container">
       <div className='youtube-cards y-cards'>
+        <Video src="https://www.youtube.com/embed/cnZuvUZSOFY"  title="Timer App with setInterval()"/>
         <Video src="https://www.youtube.com/embed/MjbC0gF22kg"  title="React Components, Props, and State"/>
         <Video src="https://www.youtube.com/embed/tLzMVZWapu8"  title="Changing Color in JavaScript"/>
         <Video src="https://www.youtube.com/embed/8PYEU-a409c"  title="CSS Smooth Transitions"/>
         <Video src="https://www.youtube.com/embed/HjMwaEwA-vs"  title="Indexing into an Array at Random"/>
+        <Video src="https://www.youtube.com/embed/hjSTjShhhH4"  title="Center a Div with HTML & CSS"/>
       </div>
     </div>
   );
